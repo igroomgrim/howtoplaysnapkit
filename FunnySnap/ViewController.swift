eec8a26a377b598723fabde2d89db441171185b6
Anak Mirasing <thaihooligan@hotmail.com>
2016-01-31 11:40:55 +0700

    Initial Xcode project

---
+++ FunnySnap/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FunnySnap
+//
+//  Created by Anak Mirasing on 1/31/2559 BE.
+//  Copyright © 2559 iGROOMGRiM. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
